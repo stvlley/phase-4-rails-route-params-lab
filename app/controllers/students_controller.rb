3a2f129530dfcf17273a4ac13089f40a6145e6b5
Stephen Talley <stvlley.amzn@gmail.com>
2022-04-04 22:07:05 -0400

    commit tree d2f606a22429fc172062758b9863c2a9b2e66f55

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,10 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  
+  def show
+    students = Student.find(params[:id])
+    render json: students
+  end
 
 end
